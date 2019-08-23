a128d883219e35cac32c6385256a5579e1b9d0d6
shamussy <e0310778@u.nus.edu>
2019-08-23 23:49:16 +0800

    Implement Add, List

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,13 +1,15 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Duke {
 
-    private String getGreeting() {
+    public String getGreeting() {
         return "Hello! I'm Duke \nWhat can I do for you?";
     }
 
-    private void getInput() {
+    public void getInput() {
         Scanner scanner = new Scanner(System.in);
+        ArrayList<String> tasklists = new ArrayList<String>();
         while(true) {
             String input = scanner.nextLine();
             if("bye".equals(input)) {
@@ -15,12 +17,22 @@ public class Duke {
                 return;
             }
             else if (!input.isEmpty() && !input.isBlank()) {
-                System.out.println(input);
+                if("list".equals(input)) {
+                    for(int i = 0; i < tasklists.size(); i++)
+                    {
+                        System.out.println(i+1 + ": " + tasklists.get(i));
+                    }
+                }
+                else {
+                    tasklists.add(input);
+                    System.out.println("added: "+input);
+                }
             }
             else {
                 System.out.println("Please do not leave any blanks");
             }
         }
+
     }
 
     public static void main(String[] args) {
