aa7957b9af8be8a0ef678a44f66637feaa21e84b
shamussy <e0310778@u.nus.edu>
2019-08-23 23:02:40 +0800

    Implement Greet, Echo, Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,31 @@
+import java.util.Scanner;
+
 public class Duke {
+
+    private String getGreeting() {
+        return "Hello! I'm Duke \nWhat can I do for you?";
+    }
+
+    private void getInput() {
+        Scanner scanner = new Scanner(System.in);
+        while(true) {
+            String input = scanner.nextLine();
+            if("bye".equals(input)) {
+                System.out.println("Bye. Hope to see you again soon!");
+                return;
+            }
+            else if (!input.isEmpty() && !input.isBlank()) {
+                System.out.println(input);
+            }
+            else {
+                System.out.println("Please do not leave any blanks");
+            }
+        }
+    }
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        Duke duke = new Duke();
+        System.out.println(duke.getGreeting());
+        duke.getInput();
     }
 }
