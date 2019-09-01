62f7aca079aa765a0cc671b56496e0230f68b64c
shamussy <e0310778@u.nus.edu>
2019-09-02 00:07:40 +0800

    Improve main to become more OOP to fulfill recommended coding style

--- src/main/java/duke/Main.java
+++ src/main/java/duke/Main.java
@@ -9,38 +9,41 @@ import duke.storage.Storage;
 import duke.task.TaskList;
 import duke.ui.Ui;
 
-public class Main {
-    public static void main(String[] args) throws DukeException {
-        Ui ui;
-        Parser parser;
-        TaskList tasks;
-        Storage storage;
-        Command command;
+class Main {
 
+    private Ui ui;
+    private Parser parser;
+    private TaskList tasks;
+    private Storage storage;
+
+    private Main() {
         ui = new Ui();
         parser = new Parser();
         storage = new Storage("data/tasks.txt");
-
         try {
             tasks = new TaskList(storage.readFile());
         } catch (FileNotFoundException e) {
             ui.printError("Could not read tasks from disk, will start with empty file");
             tasks = new TaskList();
         }
+    }
 
-        ui.greet();
-
+    private void run() {
         boolean hasExited = false;
-
+        ui.greet();
         while (!hasExited && parser.hasNextLine()) {
             try {
-                command = parser.parseLine();
+                Command command = parser.parseLine();
                 command.execute(tasks, ui, storage);
                 hasExited = command.isExit();
             } catch (DukeException exceptionMessage) {
                 ui.printError(exceptionMessage.toString());
             }
         }
+    }
+
+    public static void main(String[] args) {
+        new Main().run();
     }
 }
 
