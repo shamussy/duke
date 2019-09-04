443c858918669e2de430240e7fcafd6611ee3beb
shamussy <e0310778@u.nus.edu>
2019-09-04 15:46:12 +0800

    Implement JavaFX Tutorial 1 Hello World

--- src/main/java/duke/Main.java
+++ src/main/java/duke/Main.java
@@ -8,15 +8,20 @@ import duke.parse.Parser;
 import duke.storage.Storage;
 import duke.task.TaskList;
 import duke.ui.Ui;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.text.Font;
+import javafx.stage.Stage;
 
-class Main {
+public class Main extends Application {
 
     private Ui ui;
     private Parser parser;
     private TaskList tasks;
     private Storage storage;
 
-    private Main() {
+    public Main() {
         ui = new Ui();
         parser = new Parser();
         storage = new Storage("data/tasks.txt");
@@ -26,6 +31,15 @@ class Main {
             ui.printError("Could not read tasks from disk, will start with empty file");
             tasks = new TaskList();
         }
+    }
+
+    @Override public void start(Stage stage) throws Exception {
+        Label helloWorld = new Label("Hello World!"); // Creating a new Label control
+        helloWorld.setFont(new Font("Arial",50));
+        Scene scene = new Scene(helloWorld); // Setting the scene to be our Label
+
+        stage.setScene(scene); // Setting the stage to show our screen
+        stage.show(); // Render the stage.
     }
 
     private void run() {
